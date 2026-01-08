df9f9e6beab0b60b7b0631792c3d466fa919b6f7
temirlan <timamtr07@gmail.com>
2026-01-09 00:33:02 +0500

    new commit

--- src/Commuter.java
+++ src/Commuter.java
@@ -1,4 +1,5 @@
 import java.util.Objects;
+
 public class Commuter {
     private String name;
     private int age;
@@ -9,17 +10,22 @@ public class Commuter {
         this.age = age;
         this.hasTicket = hasTicket;
     }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
     public int getAge() { return age; }
     public void setAge(int age) { this.age = age; }
+
     public boolean isHasTicket() { return hasTicket; }
     public void setHasTicket(boolean hasTicket) { this.hasTicket = hasTicket; }
+
     @Override
     public String toString() {
         return String.format("Passenger: %s, Age: %d, Ticket: %s",
                 name, age, (hasTicket ? "Yes" : "No"));
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -27,6 +33,7 @@ public class Commuter {
         Commuter commuter = (Commuter) o;
         return age == commuter.age && Objects.equals(name, commuter.name);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
